fa316001c89e4399bf5b1fc830ef62b165d723b4
sutakki <takumi.com.orz@gmail.com>
2022-07-23 17:15:20 +0900

    グループをリスト形式で表示

--- src/components/pages/Group/GroupInfo.js
+++ src/components/pages/Group/GroupInfo.js
@@ -1,21 +1,32 @@
-import {useEffect, useState} from 'react';
+import styled from 'styled-components';
 
-import {InfoCard, InfoCardDetail, InfoCardButtons} from '../../Cards/InfoCard';
-import {Label} from '../../Utilities/Card/Label';
-import {PrimaryButton} from '../../Buttons/PrimaryButton';
-import {UsersAPI, GroupsAPI} from '../../../APILink';
+import TableRow from '@material-ui/core/TableRow';
+import TableCell from '@material-ui/core/TableCell';
+
+import {Delete, Edit} from '@material-ui/icons';
 import {Anchor} from '../../Utilities/Anchor';
 
-const GroupInfo = (props) => {
-  const [User, setUser] = useState();
 
-  useEffect(() => {
-    fetch(UsersAPI + '/' + props.data.user_id) //api
-      .then((res) => res.json())
-      .then((json) => {
-        setUser(json);
-      });
-  }, []);
+
+import Paper from '@material-ui/core/Paper';
+const StyledDelete = styled(Delete)`
+  color: red;
+  cursor: pointer;
+`;
+const DisableDelete = styled(Delete)`
+  color: #444;
+`;
+
+const StyledEdit = styled(Edit)`
+  color: green;
+  cursor: pointer;
+`;
+const GroupInfo = (props) => {
+  const onDelete = async (id) => {
+    if (confirm('対象を学習終了者としてよろしいですか？')) {
+      return await props.deleteUser(id);
+    }
+  };
 
   const Delete = async (id, name) => {
     if (confirm('グループ名：' + name + ' 本当に削除しますか？')) {
@@ -24,35 +35,27 @@ const GroupInfo = (props) => {
   };
 
   return (
-    <InfoCard>
-      <InfoCardDetail>
-        <div>
-          <Label>グループ名</Label>
-          {props.data.name}
-        </div>
-        <div>
-          <Label>作成者</Label>
-          {User ? User.name : ''}
-        </div>
-        <div>
-          <Label>アクセスキー</Label>
-          {props.data.access_key}
-        </div>
-        <div>
-          <Label>作成日</Label>
-          {props.data.created_at}
-        </div>
-      </InfoCardDetail>
-
-      <InfoCardButtons>
-        <PrimaryButton color='secondary' onClick={() => Delete(props.data.group_id, props.data.name)}>
-          削除する
-        </PrimaryButton>
+    <TableRow key={props.data.group_id} sx={{'&:last-child td, &:last-child th': {border: 0}}}>
+      <TableCell component='th' scope='row'>
+        {props.data.name}
+      </TableCell>
+      <TableCell align='center'>{props.data.summary}</TableCell>
+      <TableCell align='center'>{props.data.access_key}</TableCell>
+      <TableCell align='center'>{props.data.created_at}</TableCell>
+      <TableCell align='center'>
+        
         <Anchor to={'/group/'.concat(props.data.group_id)}>
-          <PrimaryButton>詳細を見る</PrimaryButton>
+        <StyledEdit></StyledEdit>
         </Anchor>
-      </InfoCardButtons>
-    </InfoCard>
+      </TableCell>
+      <TableCell align='center'>
+        {props.data.role !== '学習終了者' ? (
+          <StyledDelete onClick={() => onDelete(props.data.user_id)}></StyledDelete>
+        ) : (
+          <DisableDelete></DisableDelete>
+        )}
+      </TableCell>
+    </TableRow>
   );
 };
 
